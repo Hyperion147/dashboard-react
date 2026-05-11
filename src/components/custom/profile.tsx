017dc0252fc9b30a1836fa62817f6511c8ced1b8
Hyperion147 <hypercode9913@gmail.com>
2026-05-11 09:44:11 +0530

    added author reference in sidebar

--- src/components/custom/profile.tsx
+++ src/components/custom/profile.tsx
@@ -2,11 +2,16 @@ import { LogoutConfirmDialog } from "@/components/LogoutConfirmDialog";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Avatar } from "../ui/avatar";
-import { IconCode, IconLogout } from "@tabler/icons-react";
+import {
+    IconArrowUpRight,
+    IconCode,
+    IconChevronUpRight,
+} from "@tabler/icons-react";
 import { Button } from "../ui/button";
 
 const Profile = () => {
     const [showLogoutDialog, setShowLogoutDialog] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     const handleLogoutClick = () => {
         setShowLogoutDialog(true);
@@ -39,21 +44,29 @@ const Profile = () => {
                 </span>
             </a>
             <div className="h-px bg-gray-400 my-2" />
-            <div className="flex gap-4 items-center">
+            <div
+                className="flex gap-4 items-center cursor-pointer"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
                 <Avatar className="h-6 w-6">
                     <img src="/profile.jpeg" alt="" />
                 </Avatar>
                 <div className="flex justify-between w-full items-center">
                     <div>
-                        <h2 className="font-bold">Suryansu.pro</h2>
+                        <a href="https://suryansu.pro" target="_blank" className="font-bold">Suryansu.pro</a>
                         <p className="text-xs tracking-wider">Developer</p>
                     </div>
                     <Button
                         onClick={handleLogoutClick}
-                        className="cursor-pointer"
-                        variant="outline"
+                        className="cursor-pointer transition-all duration-300"
+                        variant="ghost"
                     >
-                        <IconLogout className="h-5 w-5 shrink-0" />
+                        {isHovered ? (
+                            <IconArrowUpRight className="h-10 w-10 shrink-0" />
+                        ) : (
+                            <IconChevronUpRight className="h-10 w-10 shrink-0" />
+                        )}
                     </Button>
                 </div>
             </div>
